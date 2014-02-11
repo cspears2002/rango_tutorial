c9f6d90c27e9ecc79c1d88c4c2c4f785b1f7935c
cspears2002 <cspears2002@yahoo.com>
2014-02-11 10:48:57 -0800

    Created add_category function in views.py

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
 
+from rango.forms import CategoryForm
+
 def remove_spaces(category_list):
   for category in category_list:
     category.url = category.name.replace(' ', '_')
@@ -46,3 +48,17 @@ def category(request, category_name_url):
     pass
 
   return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+  if request.method = 'POST':
+    form = CategoryForm(request.POST)
+
+    if form.is_valid():
+      form.save(commit=True)
+      return index(request)
+    else:
+      print form.errors
+  else:
+    form = CategoryForm()
+
+  return render(request, 'rango/add_category.html', {'form': form})
