2640194ff96a6cbcfc1584f375c1617c041419c8
cspears2002 <cspears2002@yahoo.com>
2014-02-10 19:22:01 -0800

    Creating a details page.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render
+
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
   # context = RequestContext(request)
@@ -14,3 +16,20 @@ def index(request):
 def about(request):
   context_dict = {'boldmessage': 'here is the about page'}
   return render(request, 'rango/about.html', context_dict)
+
+def category(request, category_name_url):
+  category_name = category_name_url.replace('_', '')
+
+  context_dict = {'category_name': category_name}
+
+  try:
+    category = Category.objects.get(name=category_name)
+
+    pages = Page.objects.filter(category=category)
+
+    context_dict['pages'] = pages
+    context_dict['category'] = category
+  except Category.DoesNotExist:
+    pass
+
+  return render(request, 'rango/category.html', context_dict)
