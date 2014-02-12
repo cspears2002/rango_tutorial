4b115ba670e5399770633a39bfc8df1ab2ced5c9
cspears2002 <cspears2002@yahoo.com>
2014-02-11 16:18:38 -0800

    Laying groundwork for user authentication.

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -48,12 +48,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'django.contrib.admin',
+    'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rango'
+    'rango',
 )
 
 MIDDLEWARE_CLASSES = (
